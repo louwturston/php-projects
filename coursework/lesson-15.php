b6e2b317fbdbb80a871a7e54fc0ff7b30c00b925
Turston Louw <83589621+louwturston@users.noreply.github.com>
2021-08-19 14:42:45 +0200

    Create lesson-15.php

---
+++ coursework/lesson-15.php
@@ -0,0 +1,25 @@
+<?php
+
+    //include('ninjas.php');
+
+    //require('ninjas.php');
+
+    //include 'ninjas.php';
+
+    //require 'ninjas.php';
+
+    //echo 'end of php';
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>PHP Tutorials</title>
+</head>
+<body>
+    <?php include('content.php'); ?>
+    <?php include('content.php'); ?>
+</body>
+</html>
